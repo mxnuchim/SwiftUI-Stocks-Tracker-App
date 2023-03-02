34a3776ee05578c1bf35b1af7fa5b52d04b3e1e4
mxnuchim <manuchimoliver779@gmail.com>
2023-03-02 09:20:56 +0100

    Initial Commit

---
+++ StocksTracker/StocksTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksTrackerApp.swift
+//  StocksTracker
+//
+//  Created by Manuchim Oliver on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StocksTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
